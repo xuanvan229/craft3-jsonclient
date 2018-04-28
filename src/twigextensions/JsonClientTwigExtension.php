335e460f1d77a2d4f200c3fd1d740f927fb2d8c7
Hong Xuan <xuanvan229@gmail.com>
2018-04-28 17:02:13 +0700

    add

--- src/twigextensions/JsonClientTwigExtension.php
+++ src/twigextensions/JsonClientTwigExtension.php
@@ -40,9 +40,12 @@ class JsonClientTwigExtension extends Twig_Extension
      * @return string
      */
     public function getCookie(){
-    	return Craft::$app->config->general->cookie;
+        return Craft::$app->config->general->cookie;
     }
 
+    public function getAuthorization() {
+        return Craft::$app->config->general->authorization;
+    }
     public function fetchJson($options = [])
     {
         //return \view::render('settings', []);
@@ -51,27 +54,137 @@ class JsonClientTwigExtension extends Twig_Extension
         if (!isset($options['url'])) {
           die('Required url parameter not set!');
         }
-	$cookie = self::getCookie();
-        $data = self::getUrl($options['url'], $cookie);
+        $authorization = self::getAuthorization();
+        $cookie = self::getCookie();
+        if ($options['check'] == "1"){
+            $data = self::getUrl($options['url'], $authorization, $options['check']);
+        }
+        if ($options['check'] == "0"){
+            $data = self::getUrl($options['url'], $authorization, $options['check']);
+        }
+        if ($options['check'] == "3"){
+            $data = self::getUrlWithId($options['url'], $authorization, $options['id'], $options['flag']);
+        }if ($options['check'] == "4"){
+            $data = self::SearchText($options['url'], $authorization, $options['text']);
+        }
+        if ($options['check'] == "2"){
+            $data = self::getUrlWithParams($options['url'], $authorization, $options['doctorName'],$options['specialty'],$options['hospital'],$options['gender'],$options['postalCode'],$options['city'],$options['state']);
+        }
+        
 
         return json_decode($data, true);
 
     }
 
-		// Function for cURL
-		private static function getUrl($url, $cookie) {
-			error_reporting(0);
+        // Function for cURL
+        private static function getUrl($url, $authorization, $check) {
+            error_reporting(0);
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            if ($check == "1"){
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: {$authorization}"));
+            }
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $store = curl_exec($ch);
+            curl_close($ch);
 
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array("Cookie: {$cookie}"));
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			$store = curl_exec($ch);
-			curl_close($ch);
+            return $store;
+        }
+        public function getUrlWithId($url, $authorization, $id, $flag){
+            $ch = curl_init();
+            error_reporting(0);
+            $query = (object) array();
+            $specialty_field = "specialty.code";
+            $query->$specialty_field = $id;
+            $flag_field = "flag";
+            $query->$flag_field = $flag;
+            $urlquery = $url.json_encode($query);
+            curl_setopt($ch, CURLOPT_URL, $urlquery);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: {$authorization}"));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $store = curl_exec($ch);     
+            curl_close($ch);
+            return $store;
+        } 
+         public function SearchText($url, $authorization, $text){
+            $ch = curl_init();
+            error_reporting(0);
+            $text_search = "";
+            $array = preg_split('/\s+/', $text, -1, PREG_SPLIT_NO_EMPTY);
+            for($x = 0; $x<count($array); $x++){
+                $text_search = $text_search.'"'.$array[$x].'"';
+            }
+            if($text !== ''){
+            $search = (object) array(
+                    '$search' => $text_search
+                 );
+            $query = (object) array();
+            $query = (object) array (
+                '$text' => $search
+                );
+            } 
+            $urlquery = $url.json_encode($query);
+            curl_setopt($ch, CURLOPT_URL, $urlquery);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: {$authorization}"));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $store = curl_exec($ch);     
+            curl_close($ch);
+            return $store;
+        } 
+        public function getUrlWithParams($url, $authorization, $doctorName, $specialty, $hospital, $gender, $postalCode, $city, $state){
+            $ch = curl_init();
+            error_reporting(0);
+            $text_search = "";
 
-			return $store;
-		}
-
+            $array = preg_split('/\s+/', $doctorName, -1, PREG_SPLIT_NO_EMPTY);
+            for($x = 0; $x<count($array); $x++){
+                $text_search = $text_search.'"'.$array[$x].'"';
+            }
+            if($doctorName !== ''){
+            $search = (object) array(
+                    '$search' => $text_search
+                 );
+            $query = (object) array();
+            $query = (object) array (
+                '$text' => $search
+            );
+            } else {
+                $query = (object) array();
+            }
+             if($specialty !== ''){
+                $specialty_field = "specialty.code";
+                $query->$specialty_field = $specialty;
+            }
+           if($hospital !== ''){
+                $hospital_field = "hospital.name";
+                $query->$hospital_field = $hospital;
+            }
+            if($gender !== ''){
+                $gender_field = "gender";
+                $query->$gender_field = $gender;
+            }
+            if($postalCode !== ''){
+                $postalCode_field = "address.postalCode";
+                $query->$postalCode_field = $postalCode;
+               
+            }
+            if($city !== ''){
+                $city_field = "address.city";
+                $query->$city_field = $city;
+            }
+            if($state !== ''){
+                 $state_field = "address.state";
+                $query->$state_field = $state;
+            }
+        
+            $urlquery = $url.json_encode($query);
+            curl_setopt($ch, CURLOPT_URL, $urlquery);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: {$authorization}"));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            $store = curl_exec($ch);     
+            curl_close($ch);
+            return $store;
+        }
 
 
 }
